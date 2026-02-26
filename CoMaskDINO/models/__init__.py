0fdb480e8823cdd0271d48436b58e499d6a9dcaf
pazifik-cyber <65894091+pazifik-cyber@users.noreply.github.com>
2026-02-26 16:13:02 +0800

    Update __init__.py

--- CoMaskDINO/models/__init__.py
+++ CoMaskDINO/models/__init__.py
@@ -1,5 +1,4 @@
 from .co_maskdino import CoMaskDINO
 from .co_maskdino_head import CoMaskDINOHead
 
-from .transformer import (CoDinoTransformer, DetrTransformerDecoderLayer,
-                          DetrTransformerEncoder, DinoTransformerDecoder)
+__all__ = ['CoMaskDINO', 'CoMaskDINOHead']
