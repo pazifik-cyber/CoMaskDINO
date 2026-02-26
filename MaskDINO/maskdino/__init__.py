aa26ae237eef402be99d493fb11d9ddb3d30538f
pazifik-cyber <65894091+pazifik-cyber@users.noreply.github.com>
2026-02-26 16:12:37 +0800

    Delete MaskDINO directory

--- MaskDINO/maskdino/__init__.py
+++
@@ -1,5 +0,0 @@
-from .maskdino import MaskDINO
-from .maskdino_fusion_head import MaskDINOFusionHead
-from .maskdino_head import MaskDINOHead
-
-__all__ = ['MaskDINO', 'MaskDINOHead', 'MaskDINOFusionHead']
